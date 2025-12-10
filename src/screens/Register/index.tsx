cff4030ab651ba7d7b632749b3bcd5fdf93ac076
Mikensonthomas142 <mikensonthomas2@gmail.com>
2025-12-09 23:27:16 -0300

    Adicionar Campo CPF

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -26,55 +26,58 @@ export default function Register() {
     return (
         <Background>
             <ScrollView showsVerticalScrollIndicator={true}>
-            <Container>
-                <StatusBar />
-                <IconPessone name="user-circle" />
-                <TitleName>MoveOn</TitleName>
-                <TitlePage>Cadastro de Usuários</TitlePage>
-                <Card>
-                    <CardInput>
-                        <Input
-                            keyboardType="text"
-                            placeholder="Nome completo" />
-                        <Celect>
-                            <Picker
-                                selectedValue={selectedValue}
-                                onValueChange={(value) => setSelectedValue(value)}
+                <Container>
+                    <StatusBar />
+                    <IconPessone name="user-circle" />
+                    <TitleName>MoveOn</TitleName>
+                    <TitlePage>Cadastro de Usuários</TitlePage>
+                    <Card>
+                        <CardInput>
+                            <Input
+                                keyboardType="text"
+                                placeholder="Nome completo" />
+                            <Input
+                                placeholder="CPF"
+                                keyboardType="phone-pad" />
+                            <Celect>
+                                <Picker
+                                    selectedValue={selectedValue}
+                                    onValueChange={(value) => setSelectedValue(value)}
+                                >
+                                    <Picker.Item label="Informe teu Sexo" value="" style={{ fontSize: 20 }} />
+                                    <Picker.Item label="Masculino" value="masculino" style={{ fontSize: 20 }} />
+                                    <Picker.Item label="Feminino" value="feminino" style={{ fontSize: 20 }} />
+                                </Picker>
+
+                            </Celect>
+                            <Input
+                                placeholder="Email"
+                                keyboardType="email-address"
+                            />
+                            <Input
+                                placeholder="Telefone"
+                                keyboardType="phone-pad"
+
+                            />
+                            <Input
+                                placeholder="Senha"
+                                secureTextEntry={true}
+                            />
+                        </CardInput>
+                        <ButtonRegister>
+                            <ButtonText>Registrar</ButtonText>
+                        </ButtonRegister>
+
+                        <ButtonLogin>
+                            <ButtonTextLogin
+                                onPress={() => navigation.navigate("Login")}
                             >
-                                <Picker.Item label="Informe teu Sexo" value="" style={{fontSize: 20}}/>
-                                <Picker.Item label="Masculino" value="masculino" style={{fontSize: 20}}/>
-                                <Picker.Item label="Feminino" value="feminino" style={{fontSize: 20}}/>
-                            </Picker>
+                                Tenho Conta
+                            </ButtonTextLogin>
+                        </ButtonLogin>
 
-                        </Celect>
-                        <Input
-                            placeholder="Email"
-                            keyboardType="email-address"
-                        />
-                        <Input
-                            placeholder="Telefone"
-                            keyboardType="phone-pad"
-
-                        />
-                        <Input
-                            placeholder="Senha"
-                            secureTextEntry={true}
-                        />
-                    </CardInput>
-                    <ButtonRegister>
-                        <ButtonText>Registrar</ButtonText>
-                    </ButtonRegister>
-
-                    <ButtonLogin>
-                        <ButtonTextLogin
-                            onPress={() => navigation.navigate("Login")}
-                        >
-                            Tenho Conta
-                        </ButtonTextLogin>
-                    </ButtonLogin>
-
-                </Card>
-            </Container >
+                    </Card>
+                </Container >
             </ScrollView>
         </Background>
     )
